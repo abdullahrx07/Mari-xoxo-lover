a71c1c5903306b7105e5b653c470aa42e1126a61
⟶⚘Nı̄̈͜ı̄̈ībbʌ Oīı̄̈͜ı̄̈īx𝆺꯭𝅥⎯ <abdullahrx96@gmail.com>
2026-07-01 20:14:55 +0600

    Add firebase-config.js via Zip Uploader

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// 👇 Ei config ta Firebase console theke nibe (FREE)
+// Steps: firebase-config-README.txt file ta dekho
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
